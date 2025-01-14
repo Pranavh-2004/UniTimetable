5f1bb53b20d92dce6606e0ac0f4ae1cd748a190f
Pranavh-2004 <pranavh2004@gmail.com>
2025-01-14 11:35:52 +0530

    Initial Commit

---
+++ UniTimetable/UniTimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniTimetableApp.swift
+//  UniTimetable
+//
+//  Created by Pranav Hemanth on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct UniTimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniTimetableTests/UniTimetableTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniTimetableTests.swift
+//  UniTimetableTests
+//
+//  Created by Pranav Hemanth on 14/01/25.
+//
+
+import Testing
+@testable import UniTimetable
+
+struct UniTimetableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
